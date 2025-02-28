f9a1cbd37fd7beb1395cd08a2f689a19783c3704
nBGD <bberghie@gmail.com>
2025-03-01 00:21:59 +0100

    Base app and delete TODO

--- todo-app-ts/src/App.tsx
+++ todo-app-ts/src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { JSX, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Todos } from './components/Todos'
+
+
+const mockTodos = [
+  {
+    id: '0',
+    title: 'Todo 0',
+    completed: false
+  },
+  {
+    id: '1',
+    title: 'Todo 1',
+    completed: false
+  },
+  {
+    id: '2',
+    title: 'Todo 2',
+    completed: true
+  }
+]
+
+const App = (): JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = (id: string) =>{
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='todoapp'>
+      <Todos 
+        onRemoveTodo={handleRemove}
+        todos={todos}/>
+    </div>
   )
 }
 
